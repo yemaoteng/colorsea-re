642b871996b3c81a3f925e7e1d5e1222528af0d5
YeMaoteng <32258494+yemaoteng@users.noreply.github.com>
2018-08-14 10:48:54 +0800

    复制粘贴Color行了！问题在Color

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import StarRating from './StarRating';
 
-const Color = ({title,color,rating=0,onRate=f=>f,onRemove=f=>f}) => {
-    return (
-        <div className="colorDiv">
-            <h3>{title}</h3>
-            <button onClick={onRemove}>X</button>
-            <div className="colorBlock" style={{backgroundColor:color}}></div>
-            <StarRating starSeledted={rating} onRate={onRate} />
-        </div>
-    )
+const Color = ({title,color,rating=0,onRemove=f=>f,onRate=f=>f}) => {
+  return (
+    <section className="color_section">
+      <h1>{title}</h1>
+      <button onClick={onRemove}>X</button>
+      <div className="color" style={{backgroundColor:color}}></div>
+      <div>
+          <StarRating starsSelected={rating} onRate={onRate} />
+      </div>
+    </section>
+  )
 }
 
 export default Color
